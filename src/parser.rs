5fe07ff73c3a3e4b1aa54192d5e083adee336e1e
dipan chakraborty <dipan2003.dev@gmail.com>
2026-02-06 07:38:38 +0000

    rewrote the parser algorithm

--- src/parser.rs
+++ src/parser.rs
@@ -3,51 +3,37 @@ use std::io;
 pub fn parse(commands: String) -> Result<Vec<String>, io::Error> {
     let commands = commands.trim().as_bytes();
     let mut pos = 0;
+
+    let mut current = Vec::new();
+    let mut inside_single_quote = false;
     let mut args = Vec::new();
 
-    let mut cmd = Vec::new();
-
-    while pos < commands.len() && commands[pos] != b' ' {
-        cmd.push(commands[pos]);
-        pos += 1;
-    }
-
-    args.push(String::from_utf8(cmd).unwrap());
-    //skip the space
-    pos += 1;
-
     while pos < commands.len() {
-        //if contains : ' something  '
-        if commands[pos] == b'\'' {
-            let mut argument = Vec::new();
-            pos += 1;
-            while pos < commands.len() && commands[pos] != b'\'' {
-                argument.push(commands[pos]);
+        match commands[pos] {
+            b'\'' => {
+                inside_single_quote = true;
                 pos += 1;
-            }
-            let str_arg = String::from_utf8(argument).unwrap();
-            if str_arg != "" {
-                args.push(str_arg);
+                continue;
             }
 
-            pos += 1;
-        } else {
-            if commands[pos] == b' ' {
-                pos += 1;
-            } else {
-                let mut argument = Vec::new();
-                while pos < commands.len() && commands[pos] != b' ' {
-                    if commands[pos] == b'\'' {
-                        pos += 1;
-                        continue;
-                    }
-
-                    argument.push(commands[pos]);
-                    pos += 1;
+            b' ' => {
+                if !current.is_empty() {
+                    args.push(String::from_utf8(current.clone()).unwrap());
+                    current.clear();
                 }
-                args.push(String::from_utf8(argument).unwrap());
+                pos += 1;
+            }
+
+            c => {
+                current.push(c);
+                pos += 1;
             }
         }
     }
+
+    args.push(String::from_utf8(current.clone()).unwrap());
+    current.clear();
+
+    println!("{:?}", args);
     Ok(args)
 }
