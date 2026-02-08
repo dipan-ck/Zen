bc8846ddcf508074203155134b9db1a17904975e
dipan chakraborty <dipan2003.dev@gmail.com>
2026-02-08 18:34:41 +0000

    added detailed comments for the tokenization algorithm

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,7 +2,10 @@ use std::io;
 
 /*
 
- It took me a good amount of time and lot of debugging to come up with this tokenization algorithm it may not be perfect or fully efficient but it works but i still decided to add detailed comments throughout the algorithm so that anyone reading this in future can atleast understand my approach and maybe make it even better and those comments will also help me if I forget how i wrote this logic which happens rarely but who knows!!!!
+ It took me a good amount of time and lot of debugging to come up with this tokenization algorithm it may not be perfect or
+fully efficient but it works but i still decided to add detailed comments throughout the algorithm so that anyone reading
+this in future can atleast understand my approach and maybe make it even better and those comments will also help me
+if I forget how i wrote this logic which happens rarely but who knows!!!!
 
 */
 
@@ -18,9 +21,9 @@ pub fn tokenize(commands: String) -> Result<Vec<String>, io::Error> {
     while pos < commands.len() {
         match commands[pos] {
             /*
-            Inside single quotes no characters has special meaning so we push every byte to the current Vector but we first
-            also check that are we already inside a double quote, if yes then the hole logic of single
-            quote doesnt apply. the logic of double quotes will be applied
+            Inside single quotes no characters has special meaning so we push every byte to the current
+            Vector but we first also check that are we already inside a double quote, if yes then the hole
+            logic of single quote doesnt apply. the logic of double quotes will be applied
             */
             b'\'' => {
                 if !inside_double_quote {
@@ -33,7 +36,10 @@ pub fn tokenize(commands: String) -> Result<Vec<String>, io::Error> {
                 }
             }
             /*
-              when we face a double quote first we check if we are already inside a single quote if yes then the rules of single quote will be followed and even the double quote will be pushed to the current vector because as we know in single quote everything preserves but if not inside single quote we set the inside double quote bool to true so that we will process next bytes based on double quotes rules.
+              when we face a double quote first we check if we are already inside a single quote if yes then the
+            rules of single quote will be followed and even the double quote will be pushed to the current vector
+            because as we know in single quote everything preserves but if not inside single quote we set the
+            inside double quote bool to true so that we will process next bytes based on double quotes rules.
             */
             b'"' => {
                 if !inside_single_quote {
@@ -46,7 +52,14 @@ pub fn tokenize(commands: String) -> Result<Vec<String>, io::Error> {
                 }
             }
             /*
-              when we face a space  or tab we first check that are we inside a single quote or double quote if yes then we preserve the space by pushing  it to the current vector because as we already know that both single quote and double quote preserves white space inside them. but if not inside any quote we check if current is empty or not if empty we wil do nothing because nothing is there to process but if there is that means we already read a argument because as we know argument are whitespaced ex: echo hello world. so when we space after hello that means the current vector already holds the bytes to convert to string to create the hello string so we push it to the args.
+              when we face a space  or tab we first check that are we inside a single quote or double
+            quote if yes then we preserve the space by pushing  it to the current vector because as we
+            already know that both single quote and double quote preserves white space inside them. but
+            if not inside any quote we check if current is empty or not if empty we wil do nothing because
+            nothing is there to process but if there is that means we already read a argument because as we
+            know argument are whitespaced ex: echo hello world. so when we space after hello that means the
+            current vector already holds the bytes to convert to string to create the hello string so we push
+            it to the args.
             */
             b' ' | b'\t' => {
                 if inside_single_quote || inside_double_quote {
@@ -61,7 +74,12 @@ pub fn tokenize(commands: String) -> Result<Vec<String>, io::Error> {
                 pos += 1;
             }
             /*
-            Now when we face a backslash first we check if we are inside a single quote because inside single quote even backslash doesnt have that escape the next character behaviour its treated as a backspash and pushed the the current vector. but if we are not inside a single quote then we need to ingore the backslash but push the next character to backslash to the vector doesn't mattter what it is we push it and because in this scenario we faced both the backslash and the next character we increment the pos by  2
+            Now when we face a backslash first we check if we are inside a single quote because inside
+            single quote even backslash doesnt have that escape the next character behaviour its treated
+            as a backspash and pushed the the current vector. but if we are not inside a single quote then
+            we need to ingore the backslash but push the next character to backslash to the vector doesn't
+            mattter what it is we push it and because in this scenario we faced both the backslash and the
+            next character we increment the pos by  2
             */
             b'\\' => {
                 if inside_single_quote {
@@ -74,7 +92,8 @@ pub fn tokenize(commands: String) -> Result<Vec<String>, io::Error> {
             }
 
             /*
-            Reaching to this match means this is just a simple character has no special meaning so we wil just process it by pushihg it to the current vector. and incrementing the pos by 1.
+            Reaching to this match means this is just a simple character has no special meaning so we wil
+            just process it by pushihg it to the current vector. and incrementing the pos by 1.
             */
             c => {
                 current.push(c);
@@ -84,7 +103,9 @@ pub fn tokenize(commands: String) -> Result<Vec<String>, io::Error> {
     }
 
     /*
-    At last we check the current vector if it's empty or not if yes that means we processed all argumennts and pushed them to the args array if not empty that measn the last  characters we matched and psuhed to current which is technically the last argument so we push it to the args.
+    At last we check the current vector if it's empty or not if yes that means we processed all
+    argumennts and pushed them to the args array if not empty that measn the last  characters we
+    matched and psuhed to current which is technically the last argument so we push it to the args.
     */
 
     if !current.is_empty() {
